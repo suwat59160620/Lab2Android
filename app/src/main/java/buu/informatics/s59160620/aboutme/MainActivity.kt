a4ad85a152bda47330b5a69a57790f16861e1f0c
suwat59160620 <59160620@go.buu.ac.th>
2019-08-18 16:51:12 +0700

    binding 3-4

--- app/src/main/java/buu/informatics/s59160620/aboutme/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160620/aboutme/MainActivity.kt
@@ -15,6 +15,8 @@ import buu.informatics.s59160620.aboutme.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Suwat 2")
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +29,9 @@ class MainActivity : AppCompatActivity() {
             nicknameText.setOnClickListener {
                 updateNickname(it)
             }
+            this.myName = this@MainActivity.myName
         }
+
     }
 
     private fun updateNickname (view: View) {
@@ -49,7 +53,8 @@ class MainActivity : AppCompatActivity() {
     private fun addNickname(view: View) {
 
         binding.apply {
-            nicknameText.text = nicknameEdit.text
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
             nicknameEdit.visibility = View.GONE
             nicknameText.visibility = View.VISIBLE
 
---
+++ app/src/main/java/buu/informatics/s59160620/aboutme/MyName.kt
@@ -0,0 +1,3 @@
+package buu.informatics.s59160620.aboutme
+
+data class MyName(var name: String = "", var nickname: String = "")
